ff3db935d0a3bf9b9daac363aa0ed446c5ffae30
ambalasurya <142885267+ambalasurya@users.noreply.github.com>
2023-12-28 18:44:11 +0530

    Update contact.service.ts

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact';
 })
 export class ContactService {
 
-  private baseUrl="http://65.0.168.78:8081/"
+  private baseUrl="http://15.206.153.159:8082/"
 
   constructor(private httpClient:HttpClient) { }
 
